b6a49a8aa7b82ebacf94df111e08bdb3a54fc982
Adu9o9 <endtales9o9@gmail.com>
2025-10-22 14:57:14 +0530

    Movie detail screen

--- navigation/RootStackNavigator.js
+++ navigation/RootStackNavigator.js
@@ -1,11 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Tabnavigator from "./Tabnavigator";
+import MovieDetailScreen from "../src/pages/TabPages/MovieDetailScreen";
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Tabnavigator" component={Tabnavigator} />
+      <Stack.Screen name="MovieDetail" component={MovieDetailScreen} />
       {/* You can add more screens here */}
     </Stack.Navigator>
   );
